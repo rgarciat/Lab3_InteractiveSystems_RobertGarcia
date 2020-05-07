a52387a8bb6798a0122951851dfe2d2f06446389
rgarciat <46629481+rgarciat@users.noreply.github.com>
2020-05-07 20:36:16 +0200

    labb

---
+++ PoseNetOSC-Unity-master/Assets/CamMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector3 myPos;
+    public Transform myPlay;
+
+    public void Update()
+    {
+        transform.position = myPlay.position + myPos;
+    }
+}
